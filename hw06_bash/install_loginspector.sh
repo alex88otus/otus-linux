4ed0f908306b2622a1ce735575a3625cf2f9a09d
alex88otus <papagunr@gmail.com>
2020-03-01 00:01:22 +0300

    funal cut for loginspector

---
+++ hw06_bash/install_loginspector.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum install -y mailx
+sudo cp -fr /vagrant/loginspector/* /
+sudo chown -R vagrant /opt/loginspector.d /opt/loginspector.sh
+sudo chmod -R u=rwx,go-rwx /opt/loginspector.d/ /opt/loginspector.sh
+systemctl reload
+sudo systemctl enable loginspector.timer
+mkdir ~/.certs
+certutil -N -d ~/.certs
+echo -n | openssl s_client -connect smtp.gmail.com:465 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ~/.certs/gmail.crt
+certutil -A -n "Google Internet Authority" -t "C,," -d ~/.certs -i ~/.certs/gmail.crt
+echo "FINISH"
